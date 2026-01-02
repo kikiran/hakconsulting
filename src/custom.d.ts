b17f53f07facfad7de7db687c5126ac114f2255c
kikiran <kiran.m79955@gmail.com>
2026-01-02 15:18:28 +0530

    added custom css

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,5 @@
-declare module 'swiper/css';
-declare module 'swiper/css/*';
+declare module "swiper/css";
+declare module "swiper/css/*";
 
-declare module '*.css';
-declare module '*.scss';
+declare module "*.css";
+declare module "*.scss";
